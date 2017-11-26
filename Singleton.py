d619604e914393710d395ce6245d37c685e29d2b
KangziLi <183168468@qq.com>
2017-11-26 18:14:58 +0800

    Singleton

---
+++ Singleton.py
@@ -0,0 +1,9 @@
+class Solution:
+	'''
+	Singleton is a most widely used design pattern. If a class has and only has one instance at every moment, we call this design as singleton. For example, for class Mouse (not a animal mouse), we should design it in singleton.
+	You job is to implement a getInstance method for given class, return the same instance of this class every time you call this method.
+    # @return: The same instance of this class every time
+    '''
+    @classmethod
+    def getInstance(cls):
+        return cls
