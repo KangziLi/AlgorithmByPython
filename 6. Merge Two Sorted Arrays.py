0e86a77eaa557619b7cc7e9dcf0ba2f987bd5045
KangziLi <183168468@qq.com>
2017-11-29 00:09:30 +0800

    update

---
+++ 6. Merge Two Sorted Arrays.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 23:59:28 2017
+
+@author: KangziLi
+@source: lintcode - 6. Merge Two Sorted Arrays
+"""
+
+class Solution:
+    """
+    @param: A: sorted integer array A
+    @param: B: sorted integer array B
+    @return: A new sorted integer array
+    """
+    def mergeSortedArray(self, A, B):
+        A=A+B
+        A.sort()
+        return A
