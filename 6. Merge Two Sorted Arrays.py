184bea12f40cd49020b1f077af51272b9f195073
KangziLi <183168468@qq.com>
2017-11-29 00:11:00 +0800

    update

--- 6. Merge Two Sorted Arrays.py
+++ 6. Merge Two Sorted Arrays.py
@@ -8,6 +8,7 @@ Created on Tue Nov 28 23:59:28 2017
 
 class Solution:
     """
+    Merge two given sorted integer array A and B into a new sorted integer array.
     @param: A: sorted integer array A
     @param: B: sorted integer array B
     @return: A new sorted integer array
