cd52ff318ee7e024fbdc27fb7c457eae1dee0d94
KangziLi <183168468@qq.com>
2017-12-07 00:15:54 +0800

    update

---
+++ 067 Binary Tree Inorder Traversal.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  6 22:36:58 2017
+
+@author: KangziLi
+@source: lintcode - 67. Binary Tree Inorder Traversal
+"""
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+
+
+class Solution:
+    """
+    @param: root: A Tree
+    @return: Inorder in ArrayList which contains node values.
+    """
+    def inorderTraversal(self, root):
+        if root is None:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
