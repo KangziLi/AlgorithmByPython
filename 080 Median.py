4ff8ecf847550e941c238ce17f37ea6b24488e62
KangziLi <183168468@qq.com>
2017-12-08 00:13:58 +0800

    update

---
+++ 080 Median.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Dec  6 21:38:35 2017
+
+@author: KangziLi
+@source: lintcode - 80. Median
+"""
+
+class Solution:
+    """
+    Given a unsorted array with integers, find the median of it.
+    A median is the middle number of the array after it is sorted.
+    If there are even numbers in the array, return the N/2-th number after sorted.   
+    @param: : A list of integers
+    @return: An integer denotes the middle number of the array
+    """
+    def median(self, nums):
+        nums.sort()
+        k = int(len(nums)/2)
+        if l%2 == 1:
+            return nums[k]
+        else:
+            return nums[k-1]
