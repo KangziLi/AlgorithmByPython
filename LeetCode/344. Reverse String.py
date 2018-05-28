55b41640aabff1e47252181ddd8951634c72143e
KangziLi <183168468@qq.com>
2018-05-28 19:11:06 +0800

    String

---
+++ LeetCode/344. Reverse String.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 16:53:50 2018
+
+@author: Kangzi Li
+
+LeetCode: 344. Reverse String
+"""
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
